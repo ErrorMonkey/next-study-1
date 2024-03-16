f1781867278eaf2b84fe11270ec53b44937d88a8
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-03-17 02:07:32 +0900

    0317 study

---
+++ app/meals/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+function page() {
+  return (
+    <>
+      <h1>meals</h1>
+      <Link href='/community'>test</Link>
+    </>
+  );
+}
+
+export default page;
