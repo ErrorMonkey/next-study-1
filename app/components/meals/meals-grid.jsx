706a64b688d59cc9fc13f62337df4e9ad52cc07d
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-03-22 23:03:37 +0900

    0322 next study

--- app/components/meals/meals-grid.jsx
+++ app/components/meals/meals-grid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import classes from './meals-grid.module.css';
 
 import MealItem from './meal-item';
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Link from 'next/link';
 
 import classes from './page.module.css';
-import Link from 'next/link';
 import MealsGrid from '../components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +26,9 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
