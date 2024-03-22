dd828c7cc715d5c5824b3635e1bd4e6b11c5f1be
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-03-22 21:10:43 +0900

    next study

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import Link from 'next/link';
 
-function page() {
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '../components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
-      <h1>meals</h1>
-      <Link href='/community'>test</Link>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favotite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 }
 
-export default page;
+export default MealsPage;
